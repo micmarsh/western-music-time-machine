735c74f5d0efb47bae4b6362d05ae8b80f18ff42
micmarsh <mike@marsh.pw>
2016-07-26 14:57:11 -0400

    untested first spec

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [org.clojure/clojurescript "1.9.89"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]
