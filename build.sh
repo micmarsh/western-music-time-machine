edb6e352153e6d4e18800e23e6b85a29e07a073d
michael <mike@marsh.pw>
2016-08-13 14:26:54 -0400

    move to edn file, break initialization

--- build.sh
+++ build.sh
@@ -2,34 +2,20 @@
 
 # npm install -g minifier
 
-rm resources/public/javascripts/map.js
-rm resources/public/css/style.css
+rm -r javascripts js css img xml edn
 
-cat ../europe/simple/js/jquery.js >> resources/public/javascripts/map.js
-echo >> resources/public/javascripts/map.js
-cat ../europe/simple/js/raphael.min.js >> resources/public/javascripts/map.js
-echo >> resources/public/javascripts/map.js
-cat ../europe/simple/js/scale.raphael.js >> resources/public/javascripts/map.js
-echo >> resources/public/javascripts/map.js
-cat ../europe/simple/js/paths.js >> resources/public/javascripts/map.js
-echo >> resources/public/javascripts/map.js
-cat ../europe/simple/js/init.js >> resources/public/javascripts/map.js
-echo >> resources/public/javascripts/map.js
+lein with-profile compile-client do clean,  cljsbuild once
 
-cat ../europe/simple/css/reset.css >> resources/public/css/style.css
-echo >> resources/public/css/style.css
-cat ../europe/simple/css/fonts.css >> resources/public/css/style.css
-echo >> resources/public/css/style.css
-cat ../europe/simple/css/style.css >> resources/public/css/style.css
-echo >> resources/public/css/style.css
-cat ../europe/simple/css/map.css >> resources/public/css/style.css
-echo >> resources/public/css/style.css
+mkdir javascripts js css img xml edn
 
-cp ../europe/simple/xml/settings.xml resources/public/xml/settings.xml
 
-perl -p -i -e "s/\r//g" resources/public/javascripts/map.js 
-perl -p -i -e "s/\r//g" resources/public/css/style.css 
-perl -p -i -e "s/\r//g" resources/public/xml/settings.xml 
+cp resources/public/img/* img/
+cp resources/public/edn/* edn/
+cp resources/public/js/client.js js/client.js
+cp resources/public/xml/settings.xml xml/settings.xml
+cp resources/public/index.html index.html
 
-# minify resources/public/javascripts/map.js > resources/public/javascripts/map.min.js
-# minify resources/public/css/style.css > resources/public/css/map.min.css
+sed -i '/<script src="javascripts\/utils.js"><\/script>/d' index.html
+
+minify resources/public/javascripts/utils.js resources/public/javascripts/map.js -o javascripts/map.js 
+minify resources/public/css/style.css -o css/style.css
