1f933c6b883dfe0adcd41c92ba1370969f4ef1b5
michael <mike@marsh.pw>
2016-11-28 17:06:37 -0500

    move data init over

--- src/cljs/western_music/core.cljs
+++ src/cljs/western_music/core.cljs
@@ -2,9 +2,32 @@
   (:require [western-music.views :refer [wmtm-app]]
             [western-music.handlers]
             [western-music.subscriptions]
-            [western-music.edn :as edn]
-            [re-frame.core :refer [dispatch dispatch-sync]]
-            [reagent.core :as reagent]))
+            [western-music.edn :refer [register-custom-readers!]]
+            [re-frame.core :refer [dispatch reg-event-fx]]
+            [reagent.core :as reagent]
+            [western-music.lib.ui :as ui]
+            [day8.re-frame.http-fx]
+            [ajax.edn :as edn]
+            [western-music.handlers.youtube]
+            [western-music.handlers.map]))
+
+(def current-seconds #(.getTime (js/Date.)))
+
+(defn cache-bust [url]
+  (->> (current-seconds)
+       (str "?&_=")
+       (str url)))
+
+(reg-event-fx
+  :initialize-data
+  ui/verify-all-data
+  (fn [_ _]
+    {:db ui/initial-data
+     :http-xhrio {:method :get
+                  :uri (cache-bust "edn/compositions.edn")
+                  :body {}
+                  :response-format (edn/edn-response-format)
+                  :on-success [:compositions-from-server]}}))
 
 (defn set-map-listeners! []
   (set! (.-onNationFocus (.-listeners js/map))
@@ -15,7 +38,7 @@
 
 (defn ^:export main
   []
-  (edn/register-custom-readers!)
+  (register-custom-readers!)
   (reagent/render [wmtm-app] (.getElementById js/document "app")
                   (fn []
                     (set-map-listeners!)
